e1164a11b95d2b8cff0a432fd506c527d2b80968
muhammadghiffari <muhammadghiffari40@gmail.com>
2023-10-02 00:16:52 +0700

    update

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,6 @@
 <div class="container-fluid d-flex flex-column justify-content-center align-items-center">
-    <h1 class="mt-4 mb-2">About Me</h1>
-    <img src="<?= BASEURL; ?>/img/ProfilePicture.png" alt="Ghiffari" width="200" class="rounded-circle shadow mb-2">
+    <br><br><br>
+    <h1 class="mt-4 mb-3">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/ProfilePicture.png" alt="Ghiffari" width="200" class="rounded-circle shadow mb-3">
     <p class="position: absolute;"</p>Halo, nama saya <strong><?=$data['nama'];?></strong>, umur saya <strong><?=$data['umur'];?></strong> tahun, saya adalah seorang <strong><?=$data['status'];?></strong>.</p>
 </div>
